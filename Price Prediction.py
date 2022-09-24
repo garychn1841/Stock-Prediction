ab063acf3aa80be943a6b4ed52e291e2530e88ae
garychn1841 <garychn1841@gmail.com>
2022-09-24 13:44:37 +0800

    update Price Prediction.py

--- Price Prediction.py
+++ Price Prediction.py
@@ -62,59 +62,59 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
 print(x_train.shape)
 
-# #Build the LSTM model
-# model = Sequential()
-# model.add(LSTM(50,return_sequences=True,input_shape=(x_train.shape[1],1)))
-# model.add(LSTM(50,return_sequences=False))
-# model.add(Dense(25))
-# model.add(Dense(1))
-#
-# #compile the model
-# model.compile(optimizer='adam',loss='mean_squared_error')
-#
-# #Train the model
-# model.fit(x_train, y_train, batch_size=1, epochs=1)
-#
-#
-# #Creat the test data set
-# #creat a new array containing scaled values from index 3793 to end
-# test_data = scaled_data[training_data_len-60: , :]
-# #creat the data sets x_test and y_test
-# x_test = []
-# y_test = dataset[training_data_len:, :]
-# for i in range(60, len(test_data)):
-#     x_test.append(test_data[i-60:i,0])
-#
-# #Convert the data to a numpy array
-# x_test = np.array(x_test)
-#
-# #Reshape the data
-# x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-#
-# #Get the model predicted price values
-# predictions = model.predict(x_test)
-# predictions = scaler.inverse_transform(predictions)
-#
-# #Get the root mean squared error(RMSE)
-# rmse = np.sqrt(np.mean(predictions - y_test)**2)
-# print(rmse)
-#
-# #Plot the data
-# train = data[:training_data_len]
-# vaild = data[training_data_len:]
-# vaild['Predictions'] = predictions
-#
-# #Visualize the data
-# plt.figure(figsize=(16,8))
-# plt.title('Model')
-# plt.xlabel('Time',fontsize=18)
-# plt.ylabel('Close Price', fontsize=18)
-# plt.plot(train['close'])
-# plt.plot(vaild[['close', 'Predictions']])
-# plt.legend(['Train','Val','Predictions'],loc='lower right')
-# plt.show()
-#
-# #Show the valid and prediction prices
-# print(vaild)
+#Build the LSTM model
+model = Sequential()
+model.add(LSTM(50,return_sequences=True,input_shape=(x_train.shape[1],1)))
+model.add(LSTM(50,return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+#compile the model
+model.compile(optimizer='adam',loss='mean_squared_error')
+
+#Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+
+#Creat the test data set
+#creat a new array containing scaled values from index 3793 to end
+test_data = scaled_data[training_data_len-60: , :]
+#creat the data sets x_test and y_test
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i,0])
+
+#Convert the data to a numpy array
+x_test = np.array(x_test)
+
+#Reshape the data
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+#Get the model predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+#Get the root mean squared error(RMSE)
+rmse = np.sqrt(np.mean(predictions - y_test)**2)
+print(rmse)
+
+#Plot the data
+train = data[:training_data_len]
+vaild = data[training_data_len:]
+vaild['Predictions'] = predictions
+
+#Visualize the data
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Time',fontsize=18)
+plt.ylabel('Close Price', fontsize=18)
+plt.plot(train['close'])
+plt.plot(vaild[['close', 'Predictions']])
+plt.legend(['Train','Val','Predictions'],loc='lower right')
+plt.show()
+
+#Show the valid and prediction prices
+print(vaild)
 
 
